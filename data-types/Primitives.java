3408acf06d111b111db9c04d6d8f67fec22ba56d
Imane Erguiti <imane.erguiti@oracle.com>
2024-03-30 02:51:21 +0000

    Add Java primitive types

---
+++ data-types/Primitives.java
@@ -0,0 +1,13 @@
+package data;
+
+class Primitives {
+    public static void main(String[] args) {
+        int myInt = 2;                      //Integer (whole number)
+        float myFloat= 2.7f;                //Floating point number
+        char myChar = 'c';                  //Character
+        boolean myBoolean = true;           //Boolean
+        String myString = "hello there";    //String
+
+        System.out.printf("integer: %d%nfloat: %f%nchar: %c%nboolean: %b%nString: %s",myInt,myFloat,myChar,myBoolean,myString);
+    }
+}
